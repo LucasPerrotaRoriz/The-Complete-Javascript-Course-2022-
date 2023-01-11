596e803726fca66b65f7e84c2ab25db61da06faa
LucasPerrotaRoriz <LucasPR1998@protonmail.com>
2023-01-11 19:13:21 -0300

    AJAX nested callback

---
+++ Seção 16 - Asynchronous Javascript - Promises, Async-Await, and AJAX/2-AJAX-Nested-Callback/starter/script.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+/* 
+Tasks 
+
+1 - Use the previous code 
+2 - Make a renderCountryData which has the html template literal and inserts the element
+3 - Get the neighbour by using the border in the response
+4 - Return if neighbour is invalid 
+5 - Make the API call for the neighbour country and do the same things like the country 1
+6 - Pass a class to the renderCountry function called 'neighbour'
+*/
+
+const renderCountry = function (data, className = '') {
+  const html = `<article class="country ${className}">
+    <img class="country__img" src="${data.flags.svg}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name.common}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${
+        Object.values(data.languages)[0]
+      }</p>
+      <p class="country__row"><span>💰</span>${
+        Object.values(data.currencies)[0].name
+      }</p>
+    </div>
+  </article>`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  // AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // Render country 1
+    renderCountry(data);
+
+    // Get neighbour country 2
+    const [neighbour] = data.borders;
+
+    if (!neighbour) return;
+
+    // AJAX call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const [data2] = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour');
+    });
+  });
+};
+
+getCountryAndNeighbour('portugal');
